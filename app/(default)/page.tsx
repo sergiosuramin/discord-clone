db5b553ac822a7f800d4cc1e653288c0eee18d9d
sergiosuramin <sergio.suramin097@gmail.com>
2024-02-17 21:12:53 +0700

    test commit: removed some code

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -2,7 +2,6 @@
 import { UserButton } from '@clerk/nextjs'
 
 import { ModeToggle } from '@/components/mode-toggle'
-import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
@@ -10,7 +9,6 @@ export default function Home() {
       <div className="dark:tw-bg-indigo-500 tw-text-indigo-300 dark:tw-text-3xl dark:tw-text-yellow-300">
         intializing: discord clone practice
       </div>
-      <Button onClick={() => alert('hehe')}>test</Button>
       <UserButton afterSignOutUrl="/" />
       <ModeToggle />
     </div>
