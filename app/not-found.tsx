a9e185ee6bf172a19b7a7e882da623889fbade66
sergiosuramin <sergio.suramin097@gmail.com>
2024-03-01 15:48:27 +0700

    feat: add custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { XOctagon } from 'lucide-react'
+import Head from 'next/head'
+import Link from 'next/link'
+
+const NotFound = () => (
+  <div className="tw-w-screen tw-h-screen tw-flex tw-justify-center tw-items-center">
+    <Head>
+      <title>404 Not Found</title>
+      <meta name="description" content="Oops! Looks like you're lost or the page is not found." />
+    </Head>
+
+    <div className="tw-grid tw-gap-y-4 tw-text-center tw-max-w-[400px]">
+      <div className="tw-flex tw-justify-center tw-mb-6">
+        <XOctagon size={96} className="tw-text-destructive" />
+      </div>
+      <h1 className="tw-font-semibold">Oops! Looks like you&apos;re lost.</h1>
+      <h3>Let&apos;s go back to our app</h3>
+
+      <Link href="/" className="tw-bg-indigo-500 hover:tw-bg-indigo-500/90 tw-text-white tw-rounded-md tw-px-4 tw-py-2">
+        Go Back
+      </Link>
+    </div>
+  </div>
+)
+
+export default NotFound
