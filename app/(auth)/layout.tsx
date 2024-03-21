d22f486166ff99e043f99bc4db38000abc5c1304
sergiosuramin <sergio.suramin097@gmail.com>
2024-03-22 04:05:52 +0700

    removed unwanted space

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from '@/components/feature/mode-toggle'
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="tw-h-svh tw-bg-gradient-primary  tw-flex tw-flex-col">
+    <div className="tw-h-svh tw-bg-gradient-primary tw-flex tw-flex-col">
       <div className="tw-ml-auto tw-pr-4 tw-py-2">
         <ModeToggle auth />
       </div>
