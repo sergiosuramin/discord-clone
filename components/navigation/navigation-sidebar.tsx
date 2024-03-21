d471e49d1a39508f82cc005c7598b4a577c070e6
sergiosuramin <sergio.suramin097@gmail.com>
2024-03-22 02:04:21 +0700

    refactor: removed footer in user button pop over

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -55,6 +55,9 @@ const NavigationSidebar = async () => {
               userButtonPopoverCard: {
                 pointerEvents: 'initial',
               },
+              userButtonPopoverFooter: {
+                display: 'none',
+              },
             },
           }}
         />
