a46f8dab09c8f503abbfb9c1d25fbd6af9249b26
sergiosuramin <sergio.suramin097@gmail.com>
2024-03-24 14:00:08 +0700

    fix: fixed SSR hydrate when logout

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,16 +1,5 @@
-'use client'
-
 import { SignIn } from '@clerk/nextjs'
-import { useEffect, useState } from 'react'
 
 export default function Page() {
-  const [isMounted, setIsMounted] = useState<boolean>(false)
-
-  useEffect(() => {
-    setIsMounted(true)
-  }, [])
-
-  if (!isMounted) return null
-
   return <SignIn />
 }
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,18 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
 import { ModeToggle } from '@/components/feature/mode-toggle'
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  const [isMounted, setIsMounted] = useState<boolean>(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) return null
+
   return (
     <div className="tw-h-svh tw-bg-gradient-primary tw-flex tw-flex-col">
       <div className="tw-ml-auto tw-pr-4 tw-py-2">
