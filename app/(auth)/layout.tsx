01289acb2e28f0dba4b15af9da41bbf3d8e5f158
sergiosuramin <sergio.suramin097@gmail.com>
2024-03-28 14:38:41 +0700

    feat: added note on login layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -14,12 +14,24 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
   if (!isMounted) return null
 
   return (
-    <div className="tw-h-svh tw-bg-gradient-primary tw-flex tw-flex-col">
+    <div className="tw-min-h-svh tw-bg-gradient-primary tw-flex tw-flex-col">
       <div className="tw-ml-auto tw-pr-4 tw-py-2">
         <ModeToggle auth />
       </div>
 
-      <div className="tw-flex-1 tw-flex tw-items-center tw-justify-center">{children}</div>
+      <div className="tw-flex-1 tw-flex tw-flex-col tw-items-center tw-justify-center">
+        {children}
+        <div className="tw-border tw-rounded-lg tw-border-orange-700 dark:tw-border-orange-400 tw-max-w-[400px] tw-p-4 tw-my-4 tw-mx-2">
+          <p>Dear Valued User,</p>
+          <br />
+          <span>
+            Feel free to login using your account. You&apos;re encouraged to ask Sergio any questions you may have,
+            including the option to{' '}
+            <span className="tw-font-semibold tw-text-rose-700 dark:tw-text-rose-500">delete</span> your account should
+            you feel uneasy about maintaining an active presence on this app.
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
